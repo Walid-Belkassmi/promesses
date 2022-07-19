d27e5968f9195ebf0f045965b5e810d8987ee959
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-07-19 14:20:30 +0200

    2 done

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-const returnHello = () => {
-    const hello = "hello world"
+// const returnHello = () => {
+//     const hello = "hello world"
 
+//     return new Promise((resolve, reject) => {
+//         setTimeout( () => {
+//             if(hello){
+//                 resolve(hello)
+//             }else{
+//                 reject("impossible de l'afficher")
+//             }
+//         }, 2000)
+//     })
+// }
+
+// const waitForMyResult = async () => {
+//     try{
+//         console.log("loading...")
+//         const result = await returnHello()
+//         console.log(result)
+//     }catch(error){
+//         console.log("error")
+//     }
+// }
+
+// waitForMyResult()
+
+const isItCold = () => {
     return new Promise((resolve, reject) => {
-        setTimeout( () => {
-            if(hello){
-                resolve(hello)
+        const temperature = 2
+
+        setTimeout(() => {
+            if(temperature <= 10){
+                resolve("Il fait froid")
             }else{
-                reject("impossible de l'afficher")
+                reject("Il fait chaud")
             }
-        }, 2000)
+        }, 4000)
     })
 }
 
 const waitForMyResult = async () => {
     try{
-        console.log("loading...")
-        const result = await returnHello()
+        console.log("J'attend 4 secondes")
+        const result = await isItCold()
         console.log(result)
     }catch(error){
-        console.log("error")
+        console.log("Error")
     }
 }
 
